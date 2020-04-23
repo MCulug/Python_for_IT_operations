a9c23fb811e0baca32aafff593c17d62962178f1
MCulug <41790613+MCulug@users.noreply.github.com>
2020-04-23 11:01:58 +0300

    Update kafka_consumer_producer.py

--- kafka_consumer_producer.py
+++ kafka_consumer_producer.py
@@ -7,18 +7,18 @@ from kafka import KafkaConsumer, KafkaProducer
 class Producer(threading.Thread):
     daemon = True
     def run(self):
-        producer = KafkaProducer(bootstrap_servers='10.19.63.154:9092',
+        producer = KafkaProducer(bootstrap_servers='targethost:9092',
                                  value_serializer=lambda v: json.dumps(v).encode('utf-8'))
 
         while True:
-            producer.send('custommon', {"dataObjectID": "melihcan_test1"})    #These two lines are sending data to topic name "custommon" to target hosts 9092 port
-            producer.send('custommon', {"dataObjectID": "melihcan_test12"})
+            producer.send('custommon', {"dataObjectID": "MCulug_test1"})    #These two lines are sending data to topic name "custommon" to target hosts 9092 port
+            producer.send('custommon', {"dataObjectID": "MCulug_test12"})
             time.sleep(1)
 class Consumer(threading.Thread):
     daemon = True
 
     def run(self):
-        consumer = KafkaConsumer(bootstrap_servers='10.19.63.154:9092',        #Here is where we define consumer which listens on 9092 port
+        consumer = KafkaConsumer(bootstrap_servers='targethost:9092',        #Here is where we define consumer which listens on 9092 port
                                  auto_offset_reset='earliest',
                                  value_deserializer=lambda m: json.loads(m.decode('utf-8')))
         consumer.subscribe(['custommon'])
